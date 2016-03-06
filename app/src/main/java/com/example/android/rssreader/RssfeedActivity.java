1fe5f8e937e1e4eb1952151f151ae74c6ac364ae
Jeremy <jmanthey1@css.edu>
2016-03-05 18:24:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/rssreader/RssfeedActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.rssreader;
+//Jeremy Manthey
+//Mobile application development
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RssfeedActivity extends Activity implements MyListFragment.OnItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+    }
+
+    @Override
+    public void onRssItemSelected(String link) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        fragment.setText(link);
+    }
+
+} 
